ca0f968ef08c275bd5860aad23819e9e1438bf40
felipesalazar24 <fe.salazarv@duocuc.cl>
2026-05-02 10:58:13 -0400

    creacion de la estructura de cada microservicio

---
+++ Backend/ms_api_gateway/src/test/java/cl/sanos_y_salvos/ms_base/MsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sanos_y_salvos.ms_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
