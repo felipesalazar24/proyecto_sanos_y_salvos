fbca3da9c0e7de1a23fbb1a742fa2b8b85e77726
felipesalazar24 <fe.salazarv@duocuc.cl>
2026-05-16 05:41:03 -0400

    solucion de error de auth

--- Backend/ms_auth/src/main/java/cl/sanos_y_salvos/ms_base/api/controller/AuthController.java
+++ Backend/ms_auth/src/main/java/cl/sanos_y_salvos/ms_base/api/controller/AuthController.java
@@ -1,6 +1,7 @@
 package cl.sanos_y_salvos.ms_base.api.controller;
 
 import cl.sanos_y_salvos.ms_base.api.dto.LoginRequestDTO;
+import cl.sanos_y_salvos.ms_base.api.dto.AuthResponseDTO;
 import cl.sanos_y_salvos.ms_base.api.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,21 +18,19 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest) {
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
         try {
-            String response = authService.login(loginRequest);
+            AuthResponseDTO response = authService.login(loginRequest);
             return ResponseEntity.ok(response);
+            
         } catch (RuntimeException e) {
-        // Log para ver el error real en la consola de Docker
             System.out.println("Error en Login: " + e.getMessage());
         
-        // Si el mensaje contiene "Credenciales", mantenemos el 401
             if (e.getMessage().contains("Credenciales")) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
             }
         
-        // Si es otro error (como conexión), mandamos un 500
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
